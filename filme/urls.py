1994e5255ffb4fd40a73ecdd08a4649d71e57718
Hugo Massashi Hatori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-02-16 11:43:47 -0300

    Creating dinamic urls

--- filme/urls.py
+++ filme/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from .views import Homepage, Homefilmes, Detalhesfilme
 
 
+app_name = 'app_filme'
+
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', Homefilmes.as_view()),
-    path('filmes/<int:pk>', Detalhesfilme.as_view())
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme')
 ]
