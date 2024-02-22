3a6a1d387d1ead8159b66506b3dbb58b351e31a4
Hugo Massashi Hatori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-02-22 11:33:07 -0300

    Showing related Movies/Series
    Customizing "get_context_data" function to return "related movies/series" to html.

--- filme/views.py
+++ filme/views.py
@@ -18,3 +18,12 @@ class Detalhesfilme(DetailView):
     template_name = 'detalhesfilme.html'
     model = Filme
     # object -> 1 item do modelo (Filme)
+
+    def get_context_data(self, **kwargs):
+        # preservando as características da função original .get_context_data
+        context = super(Detalhesfilme, self).get_context_data(**kwargs)
+
+        # filtrar a minha tabela de filmes pegando os filmes cuja a categoria é igual ao do filme da página (object)
+        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[0:5]
+        context['filmes_relacionados'] = filmes_relacionados
+        return context
