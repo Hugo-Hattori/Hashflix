8f0a9af6da6a2e96ef25f6cb0649468ddcf68352
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-02-27 12:17:00 -0300

    Update homefilmes.html

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -3,11 +3,11 @@ from .models import Filme
 
 def lista_filmes_recente(request):
     # ordenando em ordem decrescente
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8]
     return {"lista_filmes_recentes": lista_filmes}
 
 
 def lista_filmes_emAlta(request):
     # ordenando em ordem decrescente
-    lista_filmes = Filme.objects.all().order_by('-visualizações')[0:10]
+    lista_filmes = Filme.objects.all().order_by('-visualizações')[0:8]
     return {"lista_filmes_emAlta": lista_filmes}
