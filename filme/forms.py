5a63fc69b376184ae9febd803744aea9b1641e78
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-03-05 17:57:34 -0300

    Debugging
    Blocking creation of different accounts with same email address.

--- filme/forms.py
+++ filme/forms.py
@@ -10,3 +10,8 @@ class CriarContaForm(UserCreationForm):
         model = Usuario
         fields = ('username', 'email', 'password1', 'password2')
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if Usuario.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este e-mail já está cadastrado em outra conta! Por favor, utilize outro endereço de e-mail.")
+        return email
