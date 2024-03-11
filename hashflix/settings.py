5243a8c90a6db074876d7c55e8699d9e5dffa913
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-03-11 18:41:38 -0300

    Update settings.py
    Trying to correct problem where Railway is unable to create database tables.

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('postgresql://postgres:uYFlgWAiUOwoojxgrngHYJcfwxWfWpEK@viaduct.proxy.rlwy.net:33766/railway')
 
 if DATABASE_URL:
     DATABASES = {
