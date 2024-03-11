d6d54edee5ca644c5dfa2feef54d14dda07e5d46
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-03-11 18:06:47 -0300

    Preparing for deployment on Railway

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h%tu#+^-*)7caj21sric7x)39m^ro3wbr=83lpfeic-9pfi@iy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,6 +87,15 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -124,6 +134,7 @@ USE_TZ = True
 
 # Configuração de imagens subidas pelo desenvolvedor
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
